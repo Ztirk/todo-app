9d1234b2b021ae3bd8159891cfc2a7c38bdf21c0
Ztirk <krit15489@gmail.com>
2025-03-13 13:59:09 +0700

    g

--- src/apis/getTodo.ts
+++ src/apis/getTodo.ts
@@ -2,6 +2,7 @@ import axiosInterceptors from '@/configs/axiosConfig'
 
 export default async function getTodo() {
   try {
+    console.log(import.meta.env.VITE_GET_TODO)
     const res = await axiosInterceptors.get(import.meta.env.VITE_GET_TODO)
     const data = res.data
 
